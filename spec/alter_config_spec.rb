8242130d213de0b15181de10ac9ae35429451425
mcornwell <mcornwell@skyscapecloud.com>
2016-12-21 11:04:10 +0000

    Standardise ruby syntax for consistency

--- spec/alter_config_spec.rb
+++ spec/alter_config_spec.rb
@@ -5,13 +5,13 @@ describe UKCloud::Vcloud::Ipsec::Configuration do
   context 'loading current configuration using the vCloud API' do
     before do
       stub_request(:post, "https://api.vcd.portal.ukcloudcloud.com/api/sessions")
-        .to_return(:status => 200, :body => {return_body: "let"}.to_xml, headers: { "CONTENT_TYPE" => "application/xml"} )
+        .to_return(:status => 200, :body => {return_body: "let"}.to_xml, :headers => { "CONTENT_TYPE" => "application/xml"} )
 
       stub_request(:get, "https://api.vcd.portal.ukcloudcloud.com/api/query?filter=name==nftxxxxxx-x&type=edgeGateway")
-        .to_return(:status => 200, body: query_response_body.to_xml, headers: { "CONTENT_TYPE" => "application/xml" } )
+        .to_return(:status => 200, :body => query_response_body.to_xml, :headers => { "CONTENT_TYPE" => "application/xml" } )
 
       stub_request(:post, "https://api.vcd.portal.ukcloudcloud.com/api/admin/edgeGateway/639f07a1-4644-40bd-9f91-e85008850398/action/configureServices")
-        .to_return(status: 202, body: configure_response_body.to_xml, headers: { "CONTENT_TYPE" => "application/xml" })
+        .to_return(:status => 202, :body => configure_response_body.to_xml, :headers => { "CONTENT_TYPE" => "application/xml" })
 
       stub_request(:get, "https://api.vcd.portal.ukcloudcloud.com/api/task/news")
         .to_return(:status => 200, :body => { status: "completed" }.to_xml, :headers => {})
@@ -25,16 +25,16 @@ describe UKCloud::Vcloud::Ipsec::Configuration do
 
   private def query_response_body
     {
-      total: 1,
-      EdgeGatewayRecord: {
-        href: "https://api.vcd.portal.skyscapecloud.com/api/admin/edgeGateway/639f07a1-4644-40bd-9f91-e85008850398"
+      :total => 1,
+      :EdgeGatewayRecord => {
+        :href => "https://api.vcd.portal.skyscapecloud.com/api/admin/edgeGateway/639f07a1-4644-40bd-9f91-e85008850398"
       }
     }
   end
 
   private def configure_response_body
     {
-      href: "http://www.bbc.co.uk/news"
+      :href => "http://www.bbc.co.uk/news"
     }
   end
 end
